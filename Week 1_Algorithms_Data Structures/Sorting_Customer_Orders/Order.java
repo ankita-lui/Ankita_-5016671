ac3c0f1d3dc65a4ea6c68cbc84ab7baf45e17b05
Ankita karmakar <143941476+ankita-lui@users.noreply.github.com>
2024-07-31 20:34:47 +0530

    Add files via upload

---
+++ Week 1_Algorithms_Data Structures/Sorting_Customer_Orders/Order.java
@@ -0,0 +1,26 @@
+package Sorting_Customer_Orders;
+public class Order implements Comparable<Order> {
+    private int oId;
+    private String cName;
+    private double tPrice;
+
+    public Order(int orderId, String customerName, double totalPrice) {
+        this.oId = orderId;
+        this.cName = customerName;
+        this.tPrice = totalPrice;
+    }
+
+    public double getTotalPrice() {
+        return tPrice;
+    }
+
+    @Override
+    public int compareTo(Order other) {
+        return Double.compare(this.tPrice, other.tPrice);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "orderId=" + oId + ", customerName='" + cName + '\'' + ", totalPrice=" + tPrice + '}';
+    }
+}
